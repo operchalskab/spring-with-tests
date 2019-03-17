c78b22c9780e0e94290f56759f4001e150ce287f
Mateusz Wręczycki <setsudan.hana@gmail.com>
2019-03-17 04:29:39 +0100

    initial commit

---
+++ src/main/java/pl/edu/wszib/springwithtests/SpringWithTestsApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.springwithtests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithTestsApplication.class, args);
+	}
+
+}
